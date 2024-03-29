407821e5ad7b5b30720c8cbaed695cd3ff4e9c5b
Sohanur20 <mdsohank168@gmail.com>
2024-03-29 22:53:39 +0600

    authentication added

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,15 @@
+
+
+const Home = () => {
+
+
+
+    return (
+        <div>
+           home 
+           
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBWAm2WC0PnFd4oFyzg1D1DJYn6Ur9Bfms",
+  authDomain: "simple-firebase-10309.firebaseapp.com",
+  projectId: "simple-firebase-10309",
+  storageBucket: "simple-firebase-10309.appspot.com",
+  messagingSenderId: "328849800421",
+  appId: "1:328849800421:web:f2e0fdf3747edcc5ffa80c"
+};
+
+// Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+ export default app ;
